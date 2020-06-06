eef1cd8b9c9790875c630088527f409ff742fcfb
Josue Arambide <josueaqp92@gmail.com>
2020-06-06 00:45:54 -0500

    Updates

---
+++ precache-manifest.b7451ee1bc3a891b4ffc8d5813359f8a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9351804706a4f5a6ab7579df82d302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564fe07e3883e82105db",
+    "url": "/static/css/main.0dacee1b.chunk.css"
+  },
+  {
+    "revision": "7d7cdee15c5f643486c0",
+    "url": "/static/js/2.ada6045c.chunk.js"
+  },
+  {
+    "revision": "6ee87c57e2a7b3f0e0b10d639394c036",
+    "url": "/static/js/2.ada6045c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564fe07e3883e82105db",
+    "url": "/static/js/main.b7e198f4.chunk.js"
+  },
+  {
+    "revision": "eb3e6c39036cc1cb7faa",
+    "url": "/static/js/runtime-main.886f1f6a.js"
+  },
+  {
+    "revision": "28181aa8283e6adb517bdaa76b4e13f4",
+    "url": "/static/media/background-img-1.28181aa8.jpg"
+  },
+  {
+    "revision": "515801b9550d564dd5f4a364ec8de53b",
+    "url": "/static/media/background-video.515801b9.jpg"
+  },
+  {
+    "revision": "caa8c3515c54719990a78ef5197b2a23",
+    "url": "/static/media/background.caa8c351.mp4"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "b39d53af295a4e08b52e34b8f5cb1a0a",
+    "url": "/static/media/ja-logo-1.b39d53af.svg"
+  },
+  {
+    "revision": "8a61d6a7ec15f35a2003a082657e2c81",
+    "url": "/static/media/ja-logo-2.8a61d6a7.svg"
+  },
+  {
+    "revision": "1385c113e74e0998166998f3cd96a73d",
+    "url": "/static/media/xamarin.1385c113.jpg"
+  }
+]);
